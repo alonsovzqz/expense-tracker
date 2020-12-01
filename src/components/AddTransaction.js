ee710375a1fdd9872bd6aeb3ccbc1beb760e7d71
Alonso Vazquez <haloanderson45@gmail.com>
2020-12-01 00:36:39 -0600

    Adding the deleteTransaction function. Add the addTransaction function. Finish the cases in the reducer.

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
+  const { addTransaction } = useContext(GlobalContext);
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+    };
+
+    addTransaction(newTransaction);
+    setText("");
+    setAmount(0);
+  };
 
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
