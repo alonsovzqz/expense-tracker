231753d0ac36d47727b5b818acb913a60f8fc056
Alonso Vazquez <haloanderson45@gmail.com>
2021-01-04 15:31:20 -0600

    Adding a helper to add commas to amounts. Adding the functions to call the API for add/remove transactions.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { numberWithCommas } from "../utils/format";
 
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
@@ -9,9 +10,9 @@ export const Transaction = ({ transaction }) => {
   return (
     <li className={borderColor}>
       {transaction.text}{" "}
-      <span>{`${sign}$${Math.abs(transaction.amount)}`}</span>
+      <span>{`${sign}$${numberWithCommas(Math.abs(transaction.amount))}`}</span>
       <button
-        onClick={() => deleteTransaction(transaction.id)}
+        onClick={() => deleteTransaction(transaction._id)}
         className="delete-btn"
       >
         x
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,9 +1,13 @@
 import React, { createContext, useReducer } from "react";
+import axios from "axios";
+
 import AppReducer from "./AppReducer";
 
 // Initial state
 const initialState = {
   transactions: [],
+  error: null,
+  loading: true,
 };
 
 // Create context
@@ -13,24 +17,70 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  const deleteTransaction = (id) => {
-    dispatch({
-      type: "DELETE_TRANSACTION",
-      payload: id,
-    });
+  const getTransactions = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/v1/transactions");
+
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: err.response.data.error,
+      });
+    }
   };
 
-  const addTransaction = (transaction) => {
-    dispatch({
-      type: "ADD_TRANSACTION",
-      payload: transaction,
-    });
+  const deleteTransaction = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/api/v1/transactions/${id}`);
+
+      dispatch({
+        type: "DELETE_TRANSACTION",
+        payload: id,
+      });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: err.response.data.error,
+      });
+    }
+  };
+
+  const addTransaction = async (transaction) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/v1/transactions",
+        transaction,
+        config
+      );
+
+      dispatch({
+        type: "ADD_TRANSACTION",
+        payload: transaction,
+      });
+    } catch (err) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: err.response.data.error,
+      });
+    }
   };
 
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
+        getTransactions,
         deleteTransaction,
         addTransaction,
       }}
